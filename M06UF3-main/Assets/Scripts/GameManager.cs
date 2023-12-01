2e9e26a28ec355436ea5df8c1797a74ff10bd952
AzureAD\HugoCifuentesBosch <hugo.cifuentes@enti.cat>
2023-12-01 17:35:17 +0100

    singelton game manager

--- M06UF3-main/Assets/Scripts/GameManager.cs
+++ M06UF3-main/Assets/Scripts/GameManager.cs
@@ -4,6 +4,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                Instance = this;
+                DontDestroyOnLoad(this.gameObject);
+            }
+        }
+    }
+
     private int id_clicked;
 
     // Update is called once per frame
